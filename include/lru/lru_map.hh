f1edda2ee4b3c067f25ddca243140699a65d892f
vicentebolea <vicente.bolea@gmail.com>
2013-01-09 04:34:11 +0100

    fix some format

--- include/lru/lru_map.hh
+++ include/lru/lru_map.hh
@@ -1,37 +1,37 @@
 // SKETCH
 /** -------------------------------------------------------
-	* @author Vicente Adolfo Bolea Sanchez
-	*         <vicente.bolea@gmail.com>,
-	*         <vicente@unist.ac.kr>
-	*
+ * @author Vicente Adolfo Bolea Sanchez
+ *         <vicente.bolea@gmail.com>,
+ *         <vicente@unist.ac.kr>
+ *
  * @file   lru_map.hh
  * @brief  Template header for lru_map ADT
-	** -------------------------------------------------------
-	*
-	* @section DESCRIPTION
-	*
-	* This is just a sketch for the LRU class 
-	*	I am wondering to use a hash table + doubly linked list
-	*	Since for the three operations that im using i 
-	*	have theses complexities:
-	*  
-	*		 - push_front: O(1)
-	*		 - pop_back:   O(1)
-	*		 - search:     O(1 + n/k) 
-	*			
-	*	These complexities will be in the best case since 
-	* the hash table will may rehash sometimes and in each 
-	* rehash will take O(n + buckets).
-	* 
-	*	The way that im implementing this LRU is the next:
-	*	 
-	*		- The LL will just store the diskpage.
-	*	 
-	*		- The hash table given a key (wish in this experiment
-	*    will be a set of number) will store pointers to each 
-	*    node of the LL. 
-	*
-	*/
+ ** -------------------------------------------------------
+ *
+ * @section DESCRIPTION
+ *
+ * This is just a sketch for the LRU class 
+ * I am wondering to use a hash table + doubly linked list
+ * Since for the three operations that im using i 
+ * have theses complexities:
+ *
+ *  - push_front: O(1)
+ *  - pop_back:   O(1)
+ *  - search:     O(1 + n/k) 
+ *
+ * These complexities will be in the best case since 
+ * the hash table will may rehash sometimes and in each 
+ * rehash will take O(n + buckets).
+ *
+ * The way that im implementing this LRU is the next:
+ *
+ *  - The LL will just store the diskpage.
+ *
+ *  - The hash table given a key (wish in this experiment
+ *    will be a set of number) will store pointers to each 
+ *    node of the LL. 
+ *
+ */
 
 #ifndef __LRU_MAP_HH_
 #define __LRU_MAP_HH_
@@ -45,26 +45,26 @@ using std::list;
 template <class key, class value>
 class Lru: public Collection {
 
-	public:
-		Lru (size_t _max) : max (_max) {}
+ public:
+  Lru (size_t _max) : max (_max) {}
 
-		void push (const key&);
-		void pop ();
+  void push (const key&);
+  void pop ();
 
-		bool find (const key&);
+  bool find (const key&);
 
 
 protect:
-		struct entry {
-			key key_n;
-			value val_n;
-			entry (const key& k, const value& v) : 
-			 key_n(k), val_n(v) {}
-		};
+  struct entry {
+   key key_n;
+   value val_n;
+   entry (const key& k, const value& v) : 
+    key_n(k), val_n(v) {}
+  };
 
-		size_t max;
-		hashTable<key, list<value>::iterator> ht;
-		list<entry> ll;
+  size_t max;
+  hashTable<key, list<value>::iterator> ht;
+  list<entry> ll;
 
 };
 
--- include/lru/lru_map.tcc
+++ include/lru/lru_map.tcc
@@ -1,13 +1,13 @@
 /** -------------------------------------------------------
-	* @author Vicente Adolfo Bolea Sanchez
-	*         <vicente.bolea@gmail.com>,
-	*         <vicente@unist.ac.kr>
-	*
+ * @author Vicente Adolfo Bolea Sanchez
+ *         <vicente.bolea@gmail.com>,
+ *         <vicente@unist.ac.kr>
+ *
  * @file   lru_map.hh
  * @brief  Template header for lru_map ADT
-	** -------------------------------------------------------
-	*
-	*/
+ ** -------------------------------------------------------
+ *
+ */
 
 #ifndef __LRU_MAP_TCC_	
 #define __LRU_MAP_TCC_	
@@ -15,26 +15,26 @@
 template <class key>
 void Lru<key>::push (const key& k, const value& v) {
 
-	if (!belong (k)) {
-		ll.push_back (k, v);
-		ht.push (k, ll.end());
-		size++
-	}
+ if (!belong (k)) {
+  ll.push_back (k, v);
+  ht.push (k, ll.end());
+  size++
+ }
 
-	if (size > max) pop();
+ if (size > max) pop();
 }
 
 template <class key>
 void Lru<key, value>::pop (const key& k) {
 
-	list<value>::iterator it = ll.begin();
-	ht.remove ((*it).key);
-	ll.pop_front();
+ list<value>::iterator it = ll.begin();
+ ht.remove ((*it).key);
+ ll.pop_front();
 }
 
 template <class key, class value>
 bool Lru<key, value>::find (const key& k) {
-	return ht.search (k);
+ return ht.search (k);
 }
 
 #endif
